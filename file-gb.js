3cdf3f0e7d3fb03bdfeaee5c7426ca369143c819
llambda <ggcode1@users.noreply.github.com>
2015-03-25 11:32:44 -0500

    Fix generator usage

--- file-gb.js
+++ file-gb.js
@@ -1,28 +1,21 @@
-var Promise = require("bluebird");
+var Promise = require("bluebird")
+var session = require('express-session')
+var Store = require('session-file-store')(session)
 
-var session = require('express-session');
-var Store = require('session-file-store')(session);
+var count = 10000
+var i = 0
+var tasks = []
 
-var count = 10000;
-var i = 0;
-var tasks = [];
-
-var store = Promise.promisifyAll(new Store());
+var store = Promise.promisifyAll(new Store())
 
 console.time('bench'+count)
 
-/*
-
-Promise.all(tasks).then(function() {
-	console.timeEnd('bench'+count);
-	process.exit(0);
-})*/
-
-
 Promise.coroutine(function* () {
+    var promises = []
 	for (var i=0; i< count; i++) {
-		yield store.setAsync('testsession'+i, {cookie: {maxAge:2000}, name: 'sample name'});
+		promises.push(store.setAsync('testsession'+i, {cookie: {maxAge:2000}, name: 'sample name'}))
 	}
-	console.timeEnd('bench'+count);
-	process.exit(0);
+	yield Promise.all(promises)
+	console.timeEnd('bench'+count)
+	process.exit(0)
 })();
