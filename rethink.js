1a0cedf87f7155b81cd154a42fef94e5f5311e0e
llambda <ggcode1@users.noreply.github.com>
2015-05-10 10:15:57 -0500

    Add rethinkdb

---
+++ rethink.js
@@ -0,0 +1,30 @@
+var Promise = require('bluebird')
+var express = require('express');
+var session = require('express-session');
+// var RDBStore = require('express-session-rethinkdb')(session);
+var RDBStore = require('connect-rethinkdb')(session);
+
+var store = Promise.promisifyAll(new RDBStore({
+  clientOptions: {
+    db: 'test',
+    host: 'localhost',
+    port: '28015'
+  },
+  table: 'session',
+  sessionTimeout: 86400000,
+  flushInterval: 60000
+}));
+
+var count = 10000
+var tasks = []
+console.time('bench'+count)
+
+for (var i = 0; i < count; i++) {
+  tasks.push(store.setAsync('testsession'+i, {cookie: {maxAge:2000, expires: new Date() }, name: 'sample name'}))
+}
+
+Promise.all(tasks, {concurrency: 1}).then(function() {
+  console.timeEnd('bench'+count);
+  process.exit();
+})
+
